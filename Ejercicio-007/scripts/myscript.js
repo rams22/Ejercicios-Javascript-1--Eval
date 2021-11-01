73e4b7c7f6e1ed78ff16e58ee2f40191a45faf51
kino <59053398+rams22@users.noreply.github.com>
2021-11-01 21:02:20 +0100

    hasta el ejercicio 8 hecho, con algunas dudas

--- Ejercicio-007/scripts/myscript.js
+++ Ejercicio-007/scripts/myscript.js
@@ -10,3 +10,13 @@
 *
 *
 ***************************************************************************************************************/
+let num1;
+let i=0;
+num1=prompt(`introduce un numero entero`)
+for (i;num1>i;i++){
+    console.log(i) 
+    document.write(i,', ')
+}
+document.write(i) //esto es para evitar que despues del ultimo numero me ponga una coma " 0,1,2, "porque quedaria feo
+
+//duda el console log me salta de lineas, deberia ir seguido o eso me pide el ejercicio
